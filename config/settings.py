c983e79265ad4286131fa469f9f9ce8b2a75321b
AppleStein <pianonono1129@gmail.com>
2022-06-19 20:06:38 +0900

    config/urls include

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bbs.apps.BbsConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -17,9 +17,8 @@ Including another URLconf
 #https://noauto-nolife.com/post/startup-django/
 
 
-from xml.etree.ElementInclude import include
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
