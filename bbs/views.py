aff0e11d8d428afb2e3d070480a0ce22d21ba5ca
AppleStein <pianonono1129@gmail.com>
2022-06-19 21:57:15 +0900

    文字を入力して保存する

--- bbs/views.py
+++ bbs/views.py
@@ -1,13 +1,25 @@
 #url https://noauto-nolife.com/post/startup-django/
 
-from textwrap import indent
-from django.shortcuts import render
+from multiprocessing import context
+from pydoc_data.topics import topics
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.views import View
+from .models import Topic
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, "bbs/index.html")
+
+        topics = Topic.objects.all()
+        context={"topics":topics}
+
+        return render(request, "bbs/index.html", context)
+
+    def post(self, request, *args, **kwargs):
+        posted = Topic(comment = request.POST["comment"])
+        posted.save()
+
+        return redirect("bbs:index")
 
 index = IndexView.as_view()
